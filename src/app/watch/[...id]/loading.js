e5f940a0552ed32afde2b94780362a3d2e9cbd81
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2024-02-03 00:15:46 +0530

    watch page, video player

---
+++ src/app/watch/[...id]/loading.js
@@ -0,0 +1,10 @@
+import CircleLoader from "@/components/circle-loader";
+import React from "react";
+
+export default function loading() {
+  return (
+    <div>
+      <CircleLoader />
+    </div>
+  );
+}
